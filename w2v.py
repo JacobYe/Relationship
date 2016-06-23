df1394692a3466368ffdad6f6b63ce94d6ec7ed9
JacobYe <jacob.mainac@gmail.com>
2016-06-23 18:04:47 +0800

    Update dataset

--- w2v.py
+++ w2v.py
@@ -7,13 +7,13 @@ w2vUrl = "http://192.168.1.27:8080/conversation/rest/word2vec/sentencevec?src="
 
 wvDict = {
     "./csv/Breakup0622-2327.csv": "./vec/Breakup0622-2327.vec",
-    "./csv/Breakup0623-509.csv": "./vec/Breakup0623-509.vec",
+    "./csv/Breakup0623-1079.csv": "./vec/Breakup0623-1079.vec",
     "./csv/Crush0622-391.csv": "./vec/Crush0622-391.vec",
-    "./csv/Crush0623-650.csv": "./vec/Crush0623-650.vec",
+    "./csv/Crush0623-850.csv": "./vec/Crush0623-850.vec",
     "./csv/Divorce0622-46.csv": "./vec/Divorce0622-46.vec",
-    "./csv/Divorce0623-350.csv": "./vec/Divorce0623-350.vec",
+    "./csv/Divorce0623-750.csv": "./vec/Divorce0623-750.vec",
     "./csv/Love0622-2100.csv": "./vec/Love0622-2100.vec",
-    "./csv/Love0623-1246.csv": "./vec/Love0623-1246.vec",
+    "./csv/Love0623-1251.csv": "./vec/Love0623-1251.vec",
     "./csv/Marriage0622-715.csv": "./vec/Marriage0622-715.vec",
     "./csv/Marriage0623-906.csv": "./vec/Marriage0623-906.vec",
     "./csv/NotA0622-4221.csv": "./vec/NotA0622-4221.vec",
@@ -21,7 +21,7 @@ wvDict = {
     "./csv/NotDBQ0622-3604.csv": "./vec/NotDBQ0622-3604.vec",
     "./csv/NotQ0622-3051.csv": "./vec/NotQ0622-3051.vec",
     "./csv/Single0622-615.csv": "./vec/Single0622-615.vec",
-    "./csv/Single0623-450.csv": "./vec/Single0623-450.vec"
+    "./csv/Single0623-843.csv": "./vec/Single0623-843.vec"
 }
 
 for k, v in wvDict.iteritems():
