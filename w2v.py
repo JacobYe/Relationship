db3885481482904a1e07eb66f470d8ce6a014b1c
JacobYe <jacob.mainac@gmail.com>
2016-06-23 14:38:06 +0800

    Update dataset files

--- w2v.py
+++ w2v.py
@@ -5,17 +5,40 @@ import json
 
 w2vUrl = "http://192.168.1.27:8080/conversation/rest/word2vec/sentencevec?src="
 
-wFile = "./csv/not_q.csv"
-vFile = "./vec/not_q.vec"
+wvDict = {
+    "./csv/Breakup0622-2327.csv": "./vec/Breakup0622-2327.vec",
+    "./csv/Breakup0623-509.csv": "./vec/Breakup0623-509.vec",
+    "./csv/Crush0622-391.csv": "./vec/Crush0622-391.vec",
+    "./csv/Crush0623-650.csv": "./vec/Crush0623-650.vec",
+    "./csv/Divorce0622-46.csv": "./vec/Divorce0622-46.vec",
+    "./csv/Divorce0623-350.csv": "./vec/Divorce0623-350.vec",
+    "./csv/Love0622-2100.csv": "./vec/Love0622-2100.vec",
+    "./csv/Love0623-1246.csv": "./vec/Love0623-1246.vec",
+    "./csv/Marriage0622-715.csv": "./vec/Marriage0622-715.vec",
+    "./csv/Marriage0623-906.csv": "./vec/Marriage0623-906.vec",
+    "./csv/NotA0622-4221.csv": "./vec/NotA0622-4221.vec",
+    "./csv/NotDBA0622-16.csv": "./vec/NotDBA0622-16.vec",
+    "./csv/NotDBQ0622-3604.csv": "./vec/NotDBQ0622-3604.vec",
+    "./csv/NotQ0622-3051.csv": "./vec/NotQ0622-3051.vec",
+    "./csv/Single0622-615.csv": "./vec/Single0622-615.vec",
+    "./csv/Single0623-450.csv": "./vec/Single0623-450.vec"
+}
 
-with open(wFile) as wf:
-    with open(vFile, 'w') as vf:
-        for line in wf:
-            sent = line.strip()
-            w2vSent = w2vUrl + sent
-            res = requests.get(w2vSent)
-            resText = res.text.strip().replace('\n', '\\n').replace('\r', '')
-            resJson = json.loads(resText)
-            resW2v = resJson.get("sentenceVector")
-            print resW2v
-            vf.write(str(resW2v) + "\n")
+for k, v in wvDict.iteritems():
+    wFile = k
+    vFile = v
+    with open(wFile) as wf:
+        with open(vFile, 'w') as vf:
+            for line in wf:
+                sent = line.strip()
+                w2vSent = w2vUrl + sent
+                res = requests.get(w2vSent)
+                resText = res.text.strip().replace(
+                    '\n', '\\n').replace('\r', '')
+                resJson = json.loads(resText)
+                resW2v = resJson.get("sentenceVector")
+                print resW2v
+                vf.write(str(resW2v) + "\n")
+
+# wFile = "./csv/not_q.csv"
+# vFile = "./vec/not_q.vec"
