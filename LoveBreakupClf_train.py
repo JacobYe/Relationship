99b0c7f412cbf90a600afb19a9fee5ff21bf098f
JacobYe <jacob.mainac@gmail.com>
2016-06-23 13:33:48 +0800

    Update Dataset and project structure

---
+++ LoveBreakupClf_train.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from keras.models import Sequential
+
+model = Sequential()
+
+# from sklearn.neighbors import NearestCentroid
+from sklearn.externals import joblib
+
+class HobbyClassifier:
+    def __init__(self):
+        trainDict = {
+            'editorial/QA_hobby.vec':0,
+            'editorial/QA_others.vec':1,
+            'like/like.vec':0,
+            'like/notlike.vec':1,
+        }
+
+        self.X_train = []
+        self.y_train = []
+        for k, v in trainDict.items():
+            with open(k,'r') as f:
+                for l in f:
+                    if l.strip() != '' and l.strip() != '\n':
+                        vec = []
+                        for x in l.split(','):
+                            vec.append(float(x))
+                        self.X_train.append(vec)
+                        self.y_train.append(v)
+
+        self.clf = NearestCentroid()
+        self.clf.fit(self.X_train, self.y_train)
+
+        joblib.dump(self.clf, 'hobby.pkl')
+
+        self.hobbyfilter = lambda x: "喜好" if x == 0 else "非喜好"
+
+    def predict(self, vec):
+        X_test = [vec]
+        pred =  self.clf.predict(X_test)
+        return self.hobbyfilter(pred[0]) 
+
+if __name__ == "__main__":
+    classifier = HobbyClassifier()
+
+    X_test = []
+    with open('testlike/like.vec', 'r') as f:
+        for l in f:
+            if l.strip() != '' and l.strip() != '\n':
+                vec = []
+                for x in l.split(','):
+                    vec.append(float(x))
+                X_test.append(vec)
+    
+    for x in X_test:
+        print classifier.predict(x)
+
+
+
