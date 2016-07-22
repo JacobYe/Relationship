34b5729d55efb898b48098a941b6c77f37f34b19
JacobYe <jacob.mainac@gmail.com>
2016-07-22 10:01:35 +0800

    Reorganise the project structure

--- LoveBreakupClf.py
+++ LoveBreakupClf.py
@@ -35,17 +35,24 @@ class LoveBreakupClf:
 def main():
     # Load test set
 
-    testList = ['./vec/Love0623-1251.vec', './vec/Breakup0623-1079.vec']
+    # testList = ['./vec/Love0623-1251.vec', './vec/Breakup0623-1079.vec']
+    testList = ['./vec/NotDBA0622-16.vec', './vec/NotA0622-4221.vec', './vec/NotQ0622-3051.vec']
     X_test = toArray(testList)
+    y_test = []
+    for i in range(len(X_test)):
+        y_test.append(0)
     # Load model
     clf = model_from_json(open('LoveBreakupClf.json').read())
     clf.load_weights('LoveBreakupClf.h5')
     clf.compile(loss='sparse_categorical_crossentropy',
-                optimizer='sgd')
+                optimizer='sgd',
+                metrics=['accuracy'])
     prediction = clf.predict_classes(X_test, batch_size=32, verbose=1)
     print prediction
     labels = generatePredict(prediction)
     print labels
+    score = clf.evaluate(X_test, y_test, batch_size=32, verbose=1)
+    print score
 
 
 def toArray(aList):
