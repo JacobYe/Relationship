6182c49390cea3f600205b05a74f821d09f4659e
JacobYe <jacob.mainac@gmail.com>
2016-07-23 13:18:21 +0800

    Update script

--- RelationshipClf_train.py
+++ RelationshipClf_train.py
@@ -44,6 +44,10 @@ def main():
     # Layer 5
     clf.add(Dense(output_dim=128, init='uniform'))
     clf.add(Activation("relu"))
+    clf.add(Dense(output_dim=128, init='uniform'))
+    clf.add(Activation("relu"))
+    clf.add(Dense(output_dim=128, init='uniform'))
+    clf.add(Activation("relu"))
     # Layer 6
     clf.add(Dense(output_dim=64, init='uniform'))
     clf.add(Activation("relu"))
@@ -53,7 +57,7 @@ def main():
     clf.compile(loss='sparse_categorical_crossentropy',
                 optimizer='sgd',
                 metrics=['accuracy'])
-    clf.fit(X_train, y_train, batch_size=32, nb_epoch=500)
+    clf.fit(X_train, y_train, batch_size=32, nb_epoch=1000)
     score = clf.evaluate(X_test, y_test, batch_size=32, verbose=1)
     print score
     clfJson = clf.to_json()
